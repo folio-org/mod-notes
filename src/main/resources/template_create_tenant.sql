11b1e67b734d14f9b7865c5887e477c578631ada
Heikki Levanto <heikki@indexdata.dk>
2017-08-07 14:52:43 +0200

    More metadata in the sql

--- src/main/resources/template_create_tenant.sql
+++ src/main/resources/template_create_tenant.sql
@@ -34,8 +34,10 @@ CREATE TABLE IF NOT EXISTS note_data (
    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
    jsonb jsonb,
    created_date date not null default current_timestamp,
-   updated_date date not null default current_timestamp
-   );
+   updated_date date not null default current_timestamp,
+   creation_date timestamp WITH TIME ZONE,
+   created_by  text
+);
 
 -- index to support @> ops, faster than jsonb_ops
 CREATE INDEX idxgin_conf ON note_data USING gin (jsonb jsonb_path_ops);
@@ -50,10 +52,65 @@ BEGIN
 END;
 $$ language 'plpgsql';
 
-CREATE TRIGGER update_date BEFORE UPDATE ON note_data FOR EACH ROW EXECUTE PROCEDURE  update_modified_column();
+CREATE TRIGGER update_date BEFORE UPDATE ON note_data
+  FOR EACH ROW EXECUTE PROCEDURE  update_modified_column();
 
 -- give the user PRIVILEGES after everything is created by script
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA myuniversity_mymodule TO myuniversity_mymodule;
 
 -- We do not need initial data. Kept here as an example. Can copy what ever kind of data
 -- COPY note_data (jsonb) FROM 'data/locales.data' ENCODING 'UTF8';
+
+-- Automagic metadata
+-- At some point this will be moved into rmb. Now just pasted here...
+
+-- auto populate the meta data schema
+
+-- on create of user record - pull creation date and creator into dedicated column - rmb makes auto-populates these fields in the md fields
+CREATE OR REPLACE FUNCTION set_md()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.creation_date = to_timestamp(NEW.jsonb->'metaData'->>'createdDate', 'YYYY-MM-DD"T"HH24:MI:SS.MS');
+  NEW.created_by = NEW.jsonb->'metaData'->>'createdByUserId';
+  RETURN NEW;
+END;
+$$ language 'plpgsql';
+CREATE TRIGGER set_md_trigger BEFORE INSERT ON myuniversity_mymodule.note_data
+   FOR EACH ROW EXECUTE PROCEDURE  set_md();
+
+-- on update populate md fields from the creation date and creator fields
+CREATE OR REPLACE FUNCTION set_md_json()
+RETURNS TRIGGER AS $$
+DECLARE
+  createdDate timestamp WITH TIME ZONE;
+  createdBy text ;
+  updatedDate timestamp WITH TIME ZONE;
+  updatedBy text ;
+  injectedId text;
+BEGIN
+  createdBy = NEW.created_by;
+  createdDate = NEW.creation_date;
+  updatedDate = NEW.jsonb->'metaData'->>'updatedDate';
+  updatedBy = NEW.jsonb->'metaData'->>'updatedByUserId';
+
+  if createdBy ISNULL then
+    createdBy = 'undefined';
+  end if;
+  if updatedBy ISNULL then
+    updatedBy = 'undefined';
+  end if;
+  if createdDate IS NOT NULL then
+-- creation date and update date will always be injected by rmb - if created date is null it means that there is no meta data object
+-- associated with this object - so only add the meta data if created date is not null -- created date being null may be a problem
+-- and should be handled at the app layer for now -- currently this protects against an exception in the db if no md is present in the json
+    injectedId = '{"createdDate":"'||to_char(createdDate,'YYYY-MM-DD"T"HH24:MI:SS.MS')||'" , "createdByUserId":"'||createdBy||'", "updatedDate":"'||to_char(updatedDate,'YYYY-MM-DD"T"HH24:MI:SS.MSOF')||'" , "updatedByUserId":"'||updatedBy||'"}';
+    NEW.jsonb = jsonb_set(NEW.jsonb, '{metaData}' ,  injectedId::jsonb , false);
+  end if;
+RETURN NEW;
+
+END;
+$$ language 'plpgsql';
+CREATE TRIGGER set_md_json_trigger BEFORE UPDATE ON myuniversity_mymodule.note_data
+  FOR EACH ROW EXECUTE PROCEDURE set_md_json();
+
+-- --- end auto populate meta data schema ------------
