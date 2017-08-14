7888544cc5e1adf07d5d705d78d0c3ebf2c06e06
Heikki Levanto <heikki@indexdata.dk>
2017-08-14 15:21:43 +0200

    Tests for most basic CRUD ops, and some error situations

--- src/test/java/org/folio/rest/impl/NotesResourceImplTest.java
+++ src/test/java/org/folio/rest/impl/NotesResourceImplTest.java
@@ -42,7 +42,13 @@ import org.junit.After;
 public class NotesResourceImplTest {
   private final Logger logger = LoggerFactory.getLogger("okapi");
   private final int port = Integer.parseInt(System.getProperty("port", "8081"));
+  private static final String LS = System.lineSeparator();
   private final Header TEN = new Header("X-Okapi-Tenant", "modnotestest");
+  private final Header USER1 = new Header("X-Okapi-USer-Id",
+    "99999999-9999-9999-9999-999999999999");
+  private final Header USER2 = new Header("X-Okapi-USer-Id",
+    "88888888-8888-8888-8888-888888888888");
+
   private final Header JSON = new Header("Content-Type", "application/json");
   private String moduleName = "mod-notes";
   private String moduleVersion = "0.1.2-SNAPSHOT";
@@ -156,6 +162,220 @@ public class NotesResourceImplTest {
       .log().ifError()
       .statusCode(201);
 
+    // Empty list of notes
+    given()
+      .header(TEN)
+      .get("/notes")
+      .then()
+      .log().ifError()
+      .statusCode(200)
+      .body(containsString("\"notes\" : [ ]"));
+
+    // Post some malformed notes
+    String bad1 = "This is not json";
+    given()
+      .header(TEN) // no content-type header
+      .body(bad1)
+      .post("/notes")
+      .then()
+      .statusCode(400)
+      .body(containsString("Content-type"));
+
+    given()
+      .header(TEN).header(JSON)
+      .body(bad1)
+      .post("/notes")
+      .then()
+      .statusCode(400)
+      .body(containsString("Json content error"));
+
+    String note1 = "{"
+      + "\"id\" : \"11111111-1111-1111-1111-111111111111\"," + LS
+      + "\"link\" : \"users/1234\"," + LS
+      + "\"text\" : \"First note\"}" + LS;
+
+    String bad2 = note1.replaceFirst("}", ")"); // make it invalid json
+    given()
+      .header(TEN).header(JSON)
+      .body(bad2)
+      .post("/notes")
+      .then()
+      .statusCode(400)
+      .body(containsString("Json content error"));
+
+    String bad3 = note1.replaceFirst("link", "badFieldName");
+    given()
+      .header(TEN).header(JSON)
+      .body(bad3)
+      .post("/notes")
+      .then()
+      .statusCode(422)
+      .body(containsString("may not be null"))
+      .body(containsString("\"link\","));
+
+    String bad4 = note1.replaceAll("-1111-", "-2-");  // make bad UUID
+    given()
+      .header(TEN).header(JSON)
+      .body(bad4)
+      .post("/notes")
+      .then()
+      .log().all()
+      .statusCode(500); // Why does it crash on this ??
+    // Files as RMB-53
+    //.statusCode(422)
+    //.body(containsString("invalid input syntax for uuid"));
+
+    // Post a good note
+    given()
+      .header(TEN).header(USER1).header(JSON)
+      .body(note1)
+      .post("/notes")
+      .then()
+      .log().ifError()
+      .statusCode(201);
+
+    // Fetch the note in various ways
+    given()
+      .header(TEN)
+      .get("/notes")
+      .then()
+      .log().all()
+      .statusCode(200)
+      .body(containsString("First note"))
+      .body(containsString("-9999-")) // CreatedBy userid in metadata
+      .body(containsString("\"total_records\" : 1"));
+
+    given()
+      .header(TEN)
+      .get("/notes/11111111-1111-1111-1111-111111111111")
+      .then()
+      .statusCode(200)
+      .body(containsString("First note"));
+
+    given()
+      .header(TEN)
+      .get("/notes/777")
+      .then()
+      .statusCode(500);  // Another funny 500-error on invalid UUIDS.
+
+    given()
+      .header(TEN)
+      .get("/notes?query=text=fiRST")
+      .then()
+      .statusCode(200)
+      .body(containsString("First note"));
+
+    String note2 = "{"
+      + "\"id\" : \"22222222-2222-2222-2222-222222222222\"," + LS
+      + "\"link\" : \"things/23456\"," + LS
+      + "\"text\" : \"Note on a thing\"}" + LS;
+
+    // Post another note
+    given()
+      .header(TEN).header(USER2).header(JSON)
+      .body(note2)
+      .post("/notes")
+      .then()
+      .log().ifError()
+      .statusCode(201);
+
+    // Get both notes a few different ways
+    given()
+      .header(TEN)
+      .get("/notes?query=text=note")
+      .then()
+      .log().all()
+      .statusCode(200)
+      .body(containsString("First note"))
+      .body(containsString("things/23456"));
+
+    // Update a note
+    String updated1 = "{"
+      + "\"id\" : \"11111111-1111-1111-1111-111111111111\"," + LS
+      + "\"link\" : \"users/1234\"," + LS
+      + "\"text\" : \"First note with a comment\"}" + LS;
+
+    given()
+      .header(TEN).header(USER2).header(JSON)
+      .body(updated1)
+      .put("/notes/22222222-2222-2222-2222-222222222222") // wrong one
+      .then()
+      .log().ifError()
+      .statusCode(422)
+      .body(containsString("Can not change the id"));
+
+    given()
+      .header(TEN).header(USER2).header(JSON)
+      .body(updated1)
+      .put("/notes/55555555-5555-5555-5555-555555555555") // bad one
+      .then()
+      .log().ifError()
+      .statusCode(422)
+      .body(containsString("Can not change the id"));
+
+    given()
+      .header(TEN).header(USER2).header(JSON)
+      .body(updated1)
+      .put("/notes/11111111-1111-1111-1111-111111111111")
+      .then()
+      .log().ifError()
+      .statusCode(204);
+
+    given()
+      .header(TEN)
+      .get("/notes/11111111-1111-1111-1111-111111111111")
+      .then()
+      .log().all()
+      .statusCode(200)
+      .body(containsString("with a comment"))
+      .body(containsString("-8888-"));   // updated by
+
+    // _self
+    given()
+      .header(TEN).header(USER1)
+      .get("/notes/_self")
+      .then()
+      .statusCode(200)
+      .body(containsString("with a comment"));
+
+    given()
+      .header(TEN).header(USER2)
+      .get("/notes/_self")
+      .then()
+      .log().all()
+      .body(containsString("with a comment")) // editedby matches
+      .body(containsString("on a thing")); // createdby matches
+
+    // delete them
+    given()
+      .header(TEN)
+      .delete("/notes/11111111-1111-1111-1111-111111111111")
+      .then()
+      .log().all()
+      .statusCode(204);
+
+    given()
+      .header(TEN)
+      .delete("/notes/11111111-1111-1111-1111-111111111111")
+      .then()
+      .statusCode(404);
+
+    given()
+      .header(TEN)
+      .delete("/notes/22222222-2222-2222-2222-222222222222")
+      .then()
+      .log().all()
+      .statusCode(204);
+
+    given()
+      .header(TEN)
+      .get("/notes")
+      .then()
+      .log().ifError()
+      .statusCode(200)
+      .body(containsString("\"notes\" : [ ]"));
+
+    // All done
     logger.info("notesTest done");
     async.complete();
   }
