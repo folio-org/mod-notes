562fcdd62945c41bdef6723ea11d5555f955e3c0
Natalia_Zaitseva <Natalia_Zaitseva1@epam.com>
2019-04-04 11:05:56 +0300

    added tests to pass Sonar quality gates

--- src/test/java/org/folio/rest/impl/NoteTypesImplTest.java
+++ src/test/java/org/folio/rest/impl/NoteTypesImplTest.java
@@ -7,10 +7,12 @@ import static org.folio.rest.impl.TestUtil.readFile;
 import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder;
 import com.github.tomakehurst.wiremock.matching.EqualToPattern;
 import com.github.tomakehurst.wiremock.matching.UrlPathPattern;
+import io.restassured.http.Header;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
+import org.apache.http.protocol.HTTP;
 import org.folio.rest.TestBase;
 import org.junit.Test;
 
@@ -20,7 +22,9 @@ import org.junit.runner.RunWith;
 @RunWith(VertxUnitRunner.class)
 public class NoteTypesImplTest extends TestBase {
 
+  private final String NOT_EXISTING_STUB_ID = "9798274e-ce9d-46ab-aa28-00ca9cf4698a";
   private final String NOTE_TYPES_ENDPOINT = "/note-types";
+  private final Header CONTENT_TYPE_HEADER = new Header(HTTP.CONTENT_TYPE, "application/json");
 
   @Test
   public void shouldReturn200WithNoteTypeWhenValidId () throws IOException, URISyntaxException {
@@ -41,18 +45,16 @@ public class NoteTypesImplTest extends TestBase {
   @Test
   public void shouldReturn404WhenInvalidId (){
 
-    final String notExistingStubId = "9798274e-ce9d-46ab-aa28-00ca9cf4698a";
-
     RestAssured.given()
       .spec(getRequestSpecification())
       .when()
-      .get(NOTE_TYPES_ENDPOINT + "/" + notExistingStubId)
+      .get(NOTE_TYPES_ENDPOINT + "/" + NOT_EXISTING_STUB_ID)
       .then()
       .statusCode(404).extract().asString();
   }
 
   @Test
-  public void shouldReturn500WhenErrorOcured (){
+  public void shouldReturn500WhenErrorOccurred (){
 
     final String invalidStubId = "11111111-222-1111-2-111111111111";
     stubFor(
@@ -66,6 +68,58 @@ public class NoteTypesImplTest extends TestBase {
       .get(NOTE_TYPES_ENDPOINT + "/" + invalidStubId)
       .then()
       .statusCode(500).extract().asString();
+  }
+
+  @Test
+  public void shouldReturn501WhenDeleteEndpointNotImplemented (){
+
+    stubFor(
+      get(new UrlPathPattern(new EqualToPattern(NOTE_TYPES_ENDPOINT + "/" + NOT_EXISTING_STUB_ID), false))
+        .willReturn(new ResponseDefinitionBuilder()
+          .withStatus(501)));
+
+    RestAssured.given()
+      .spec(getRequestSpecification())
+      .when()
+      .delete(NOTE_TYPES_ENDPOINT + "/" + NOT_EXISTING_STUB_ID)
+      .then()
+      .statusCode(501).extract().asString();
+  }
+
+  @Test
+  public void shouldReturn501WhenPostEndpointNotImplemented () throws IOException, URISyntaxException {
+
+    stubFor(
+      get(new UrlPathPattern(new EqualToPattern(NOTE_TYPES_ENDPOINT), false))
+        .willReturn(new ResponseDefinitionBuilder()
+          .withStatus(501)));
+
+    RestAssured.given()
+      .spec(getRequestSpecification())
+      .header(CONTENT_TYPE_HEADER)
+      .body(readFile("post_note_type.json"))
+      .when()
+      .post(NOTE_TYPES_ENDPOINT)
+      .then()
+      .statusCode(501).extract().asString();
+  }
+
+  @Test
+  public void shouldReturn501WhenPutEndpointNotImplemented () throws IOException, URISyntaxException {
+
+    stubFor(
+      get(new UrlPathPattern(new EqualToPattern(NOTE_TYPES_ENDPOINT+ "/" + NOT_EXISTING_STUB_ID), false))
+        .willReturn(new ResponseDefinitionBuilder()
+          .withStatus(501)));
+
+    RestAssured.given()
+      .spec(getRequestSpecification())
+      .header(CONTENT_TYPE_HEADER)
+      .body(readFile("post_note_type.json"))
+      .when()
+      .put(NOTE_TYPES_ENDPOINT + "/" + NOT_EXISTING_STUB_ID)
+      .then()
+      .statusCode(501).extract().asString();
   }
 
 }
