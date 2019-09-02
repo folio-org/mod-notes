968fa3a0aabb469de0c38b122b51005826fd8b58
Andrii Paias <Andrii_Paias@epam.com>
2019-09-02 12:50:25 +0300

    Update RMB to 27.0.0 to use "snippetPath" schema attribute
    Jira: MODNOTES-128

--- src/main/resources/templates/db_scripts/create_note_type_view.sql
+++ src/main/resources/templates/db_scripts/create_note_type_view.sql
@@ -1,9 +1,10 @@
-SELECT note_type.id,
-    jsonb_build_object('id', note_type.jsonb ->> 'id'::text,
-    'name', note_type.jsonb ->> 'name'::text,
-    'usage', json_build_object('noteTotal', count(note_data.jsonb ->> 'id'::text)),
-    'metadata', note_type.jsonb -> 'metadata'::text)
-    AS jsonb
-    FROM diku_mod_notes.note_type
-    LEFT JOIN diku_mod_notes.note_data ON (note_data.jsonb ->> 'typeId'::text) = (note_type.jsonb ->> 'id'::text)
+CREATE OR REPLACE VIEW note_type_view AS
+  SELECT note_type.id, jsonb_build_object(
+  'id', note_type.jsonb ->> 'id'::text,
+  'name', note_type.jsonb ->> 'name'::text,
+  'usage', json_build_object('noteTotal', count(note_data.jsonb ->> 'id'::text)),
+  'metadata', note_type.jsonb -> 'metadata'::text)
+  AS jsonb
+  FROM note_type
+    LEFT JOIN note_data ON (note_data.jsonb ->> 'typeId')::uuid = (note_type.id)
     GROUP BY note_type.id;
