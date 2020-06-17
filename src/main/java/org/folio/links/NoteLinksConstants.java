8d0d8d7ea93b66a37b87de146d1f56265a41a151
Natalia_Zaitseva <Natalia_Zaitseva1@epam.com>
2020-06-17 19:29:37 +0300

    MODNOTES-144 - Notes Accordion: Make Note type column heading sortable
    * updated raml file with information about ordering by noteType
    * updated existing implementation with ability to sort by noteType
    * added tests

--- src/main/java/org/folio/links/NoteLinksConstants.java
+++ src/main/java/org/folio/links/NoteLinksConstants.java
@@ -45,6 +45,8 @@ class NoteLinksConstants {
 
   static final String ORDER_BY_LINKS_NUMBER = "ORDER BY json_array_length((data.jsonb->>'links')::json) %s ";
 
+  static final String ORDER_BY_NOTE_TYPE = "ORDER BY type.jsonb->>'name' %s ";
+
   static final String LIMIT_OFFSET = "LIMIT ? OFFSET ? ";
 
   private static final String JOIN_NOTE_TYPE_TABLE = "LEFT JOIN %s as type on (data.jsonb -> 'typeId' = type.jsonb -> 'id') ";
