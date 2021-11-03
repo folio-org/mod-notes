8c8d842c99a03f116a788ee60ff8bf722cec8a12
Pavlo Smahin <Pavlo_Smahin@epam.com>
2021-11-03 12:07:46 +0200

    MODNOTES-181 Cleanup code

--- src/main/java/org/folio/notes/config/ApplicationConfig.java
+++ src/main/java/org/folio/notes/config/ApplicationConfig.java
@@ -1,16 +1,8 @@
 package org.folio.notes.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApplicationConfig {
 
-  @Bean
-  public OpenAPI openAPI() {
-    return new OpenAPI()
-      .info(new Info().title("Employee APIs").description("Description here.."));
-  }
 }
