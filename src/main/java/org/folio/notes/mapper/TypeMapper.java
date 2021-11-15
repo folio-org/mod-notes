4e006fb166a27b5d66e5f84b9b44828c7ae2d860
Shans_Kaluhin <Shans_Kaluhin@epam.com>
2021-11-15 13:13:36 +0100

    fix mapper

--- src/main/java/org/folio/notes/mapper/TypeMapper.java
+++ src/main/java/org/folio/notes/mapper/TypeMapper.java
@@ -1,7 +1,12 @@
 package org.folio.notes.mapper;
 
+import java.sql.Timestamp;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
 import java.util.List;
+import java.util.UUID;
 
+import org.apache.commons.lang3.StringUtils;
 import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -9,6 +14,7 @@ import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValueCheckStrategy;
 import org.springframework.data.domain.Page;
 
+import org.folio.notes.domain.dto.Metadata;
 import org.folio.notes.domain.dto.NoteType;
 import org.folio.notes.domain.dto.NoteTypeCollection;
 import org.folio.notes.domain.entity.TypeEntity;
@@ -35,4 +41,20 @@ public interface TypeMapper {
   @Mapping(target = "createdDate", ignore = true)
   @Mapping(target = "createdBy", ignore = true)
   TypeEntity updateNoteType(NoteType dto, @MappingTarget TypeEntity entity);
+
+  @Mapping(target = "updatedByUserId", source = "updatedBy")
+  @Mapping(target = "createdByUserId", source = "createdBy")
+  Metadata toMetadata(TypeEntity entity);
+
+  default OffsetDateTime map(Timestamp value) {
+    return value != null ? OffsetDateTime.from(value.toInstant().atZone(ZoneId.systemDefault())) : null;
+  }
+
+  default String map(UUID value) {
+    return value != null ? value.toString() : null;
+  }
+
+  default UUID map(String value) {
+    return (StringUtils.isBlank(value)) ? null : UUID.fromString(value);
+  }
 }
