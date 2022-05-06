92e083ca26bba31824e58f1049fa88e4bf1bce86
Pavlo Smahin <Pavlo_Smahin@epam.com>
2022-05-06 17:35:02 +0300

    MODNOTES-219 Fix migration when userId is undefined (#207)
    (cherry picked from commit 10c7fd4bcbdb66bae4dfb03a1e1b0ddafedb51f3)

--- src/main/resources/db/changelog/changes/v3.0.0/sql/migrate-types.sql
+++ src/main/resources/db/changelog/changes/v3.0.0/sql/migrate-types.sql
@@ -1,8 +1,11 @@
 INSERT INTO type (id, name, created_by, created_date, updated_by, updated_date)
 SELECT id,
        jsonb->>'name',
-       (jsonb->'metadata'->>'createdByUserId')::uuid,
+       CASE WHEN (jsonb->'metadata'->>'createdByUserId')::text = 'undefined' THEN NULL
+       ELSE (jsonb->'metadata'->>'createdByUserId')::uuid END,
        to_timestamp(jsonb->'metadata'->>'createdDate', 'YYYY-MM-DDTHH24:MI:SS.MS'),
-       (jsonb->'metadata'->>'updatedByUserId')::uuid,
+       CASE WHEN (jsonb->'metadata'->>'updatedByUserId')::text = 'undefined' THEN NULL
+       ELSE (jsonb->'metadata'->>'updatedByUserId')::uuid END,
        to_timestamp(jsonb->'metadata'->>'updatedDate', 'YYYY-MM-DDTHH24:MI:SS.MS')
 FROM note_type
+ON CONFLICT DO NOTHING;
