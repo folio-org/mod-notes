f1204961163a5e91be0de7ce1ecf43a4dbcca861
viacheslav_kolesnyk <Viacheslav_Kolesnyk@epam.com>
2022-05-09 17:56:23 +0200

    MODNOTES-218 Migration Issue from Kiwi-Lotus - improve note-links migration script

--- src/main/resources/db/changelog/changes/v3.0.0/sql/migrate-note-links.sql
+++ src/main/resources/db/changelog/changes/v3.0.0/sql/migrate-note-links.sql
@@ -1,5 +1,5 @@
 INSERT INTO note_link (note_id, link_id)
 SELECT n.id, l.id
 FROM note_data n
-JOIN link l ON EXISTS (SELECT FROM jsonb_array_elements(n.jsonb->'links') link WHERE link =
-					  jsonb_build_object('id',l.object_id,'type',l.object_type))
+CROSS JOIN LATERAL jsonb_array_elements(n.jsonb->'links') AS li(link)
+JOIN link l on li.link = jsonb_build_object('id',l.object_id,'type',l.object_type)
